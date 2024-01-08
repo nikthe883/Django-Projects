8e2c4528b7eae812bcf47ed0fbc6b783dbdecb26
nikthe883 <popmihaylov@gmail.com>
2024-01-08 18:34:48 +0200

    views protection

--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import auth
 
 from django.contrib.auth import authenticate, login, logout
 
+from django.contrib.auth.decorators import login_required
+
 # Homepage
 
 def homepage(request):
@@ -154,6 +156,7 @@ def user_logout(request):
 
 # Dashboard
 
+@login_required(login_url='my-login')
 def dashboard(request):
     
     return render(request,'crm/dashboard.html')
