6ab9293c2c5c2b2d31a7d2d140d82e8d2d5b4d87
nikthe883 <popmihaylov@gmail.com>
2024-01-11 17:00:57 +0200

    commit

--- thoughts/thoughts/settings.py
+++ thoughts/thoughts/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from pass_config import EMAIL_HOST_PASSWORD_CONFIG, EMAIL
+from .pass_config import EMAIL,EMAIL_HOST_PASSWORD_CONFIG
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
